4e942b206bbfa35c2338692720d6380cf05fb107
anushkabhostekar <155798041+anushkabhostekar@users.noreply.github.com>
2024-03-06 07:03:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let name="anushka"
+let age=23
+let isLoggedIn=false;
+
+
+// number => 2 to power 53
+// bignit
+// string => ""
+
+// boolean  = true / false
+// null standaalone value
+// undefined =>
+// symbol => unique 
+
+
+
+// object
+
+console.log(typeof age);
