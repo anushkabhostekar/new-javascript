97a52063dcb3ecb3a082c11aff04241577f0fb61
anushkabhostekar <155798041+anushkabhostekar@users.noreply.github.com>
2024-03-06 07:04:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountName =144553
+let accountEmail="anu@gmail.com"
+var accountPassword="1234"
+accountCity ="roha"
+
+// accountName =2 
+
+accountEmail="ansh@gmail.com"
+accountPassword="454545"
+accountCity="ghosale"
+
+console.table([accountEmail,accountPassword,accountCity])
