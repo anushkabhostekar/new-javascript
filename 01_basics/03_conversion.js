71881fd9c0c8d7d2735c0f1eb3b6787ca7d0fbbc
anushkabhostekar <155798041+anushkabhostekar@users.noreply.github.com>
2024-03-07 06:31:02 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -18,5 +18,27 @@
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ..........................operations...................................
+
+let value=3
+let negValue= -value
+// console.log(negValue);
+
+let str1= "hello"
+let str2=" Anushka"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2);
+// console.log("2" + 2 + 2);
+// console.log(2 + 2 + "2");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log
