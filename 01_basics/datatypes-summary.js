3b351adb6b536d410a88db1f0a494ec7f151c05d
anushkabhostekar <155798041+anushkabhostekar@users.noreply.github.com>
2024-03-12 04:32:57 +0000

    summary data types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// primitive datatype
+
+// 7 types : string, number, boolean , null , undefined , symbol, BigInt 
+
+
+// Reference (non-primitive)
+
+// Array, Objects, functions 
+const heros =["shaktiman","anushka","hrushi"];
+let myObj={
+    name :"hitesh",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myfunction);
